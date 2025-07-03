55fa53facaa3d4ded433d3c973b9885d41c35d17
GHUB-arnav-10 <sudarnav2003@gmail.com>
2025-07-03 21:41:10 +0530

    API addition [POSTMAN] using flask-restx

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,48 @@
-from application import app
-from flask import render_template,request,redirect,flash,url_for,session
+from application import app,api
+from flask import render_template,request,redirect,flash,url_for,session,jsonify
 from application.models import User,Course,Enrollment
 from application.forms import LoginForm,RegisterForm
+from flask_restx import Resource
+
+##################################################################
+@api.route('/api','/api/')
+class GetAndPost(Resource):
+    #GET ALL
+    def get(self):
+        return jsonify(User.objects.all())
+    
+    #POST
+    def post(self):
+        data=api.payload
+        user=User(user_id=data['user_id'],email=data['email'],first_name=data['first_name'],last_name=data['last_name'])
+        user.set_password(data['password'])
+        user.save()
+        return jsonify(User.objects(user_id=data['user_id']))
+    
+@api.route('/api/<idx>')
+class GetUpdateDelete(Resource):
+    #GET 1 (idx)
+    def get(self,idx):
+        return jsonify(User.objects.all(user_id=idx))
+    
+    #PUT
+    def put(self,idx):
+        data=api.payload
+        User.objects(user_id=idx).update(**data)
+        return jsonify(User.objects(user_id=idx))
+    
+    #DELETE
+    def delete(self,idx):
+        User.objects(user_id=idx).delete()
+        return jsonify("User is deleted!")
+
+##################################################################
+
+
+
+
+
+
 
 @app.route("/")
 @app.route("/index")
